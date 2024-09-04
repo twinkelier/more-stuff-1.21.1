c9bbd2ea7e9e225c006a2ca31df0396eceb9e457
twinkelier <tchelink@gmail.com>
2024-09-04 18:55:49 +0200

    Initial commit

---
+++ src/main/java/net/poopstinkyfunny/morestuff/MoreStuffClient.java
@@ -0,0 +1,10 @@
+package net.poopstinkyfunny.morestuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/poopstinkyfunny/morestuff/MoreStuffDataGenerator.java
@@ -0,0 +1,11 @@
+package net.poopstinkyfunny.morestuff;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreStuffDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
